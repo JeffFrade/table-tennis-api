352afc984404c1caa19254b71a0095c6520c2d70
Jefferson Frade <jefferrson.frade@gmail.com>
2019-06-09 03:00:48 -0300

    Finished MySQL database

--- app/Repositories/Models/User.php
+++ app/Repositories/Models/User.php
@@ -32,15 +32,6 @@ class User extends Authenticatable implements JWTSubject
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
     public function getJWTIdentifier()
     {
         return $this->id;
