2d19805f51683130b9f4918f9cad4592f1406a9d
Jefferson Frade <jefferrson.frade@gmail.com>
2019-06-09 02:56:31 -0300

    Configured JWT

--- app/Repositories/Models/User.php
+++ app/Repositories/Models/User.php
@@ -5,8 +5,9 @@ namespace App\Repositories\Models;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
     use SoftDeletes;
@@ -39,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->id;
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
