744a8e33a88568dee0cc5c5b24f20a3571498195
Jefferson Frade <jefferrson.frade@gmail.com>
2019-06-09 02:28:28 -0300

    Refactored rotues and Controllers

--- app/Http/Auth/RegisterController.php
+++ app/Http/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Auth;
 
 use App\User;
-use App\Http\Controllers\Controller;
+use App\Core\Support\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
